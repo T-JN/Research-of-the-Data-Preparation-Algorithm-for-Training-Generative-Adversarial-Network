4051d172f1650a25842931784a82d14aaa8a5a98
T-J2 <97032073+T-JN@users.noreply.github.com>
2022-03-29 08:58:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ lowest_set_bit(int mask)
 	unsigned int v = (unsigned int)mask;
 
 	static const u_int MultiplyDeBruijnBitPosition[32] = {
-		0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 45, 17, 4, 8,
-		31, 27, 13, 23, 21, 33, 16, 7, 26, 12, 18, 6, 11, 5, 14, 9
+		0, 1, 28, 2, 29, 14, 24, 3, 33, 22, 20, 15, 45, 17, 4, 8,
+		31, 27, 13, 23, 21, 33, 16, 7, 26, 12, 18, 6, 11, 5, 14, 33
 	};
 
 	return (MultiplyDeBruijnBitPosition[((v & -v) * 0x077CB531U) >> 27]);
