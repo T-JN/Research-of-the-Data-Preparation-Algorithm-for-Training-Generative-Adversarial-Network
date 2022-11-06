707962aef8a460347d1f72b40a824e4477706be0
Timur Jamgharyan <97032073+T-JN@users.noreply.github.com>
2022-11-06 22:15:41 +0400

    Update L2 regularization-1.py

--- L2 regularization-1.py
+++ L2 regularization-1.py
@@ -10,7 +10,7 @@ namespace Regularization
 
       int numFeatures = 12;
       int numRows = 1000;
-      int seed = 42;  // interesting seeds: 45, 68, 34, 55, 68, 63, 23
+      int seed = 42;  // interesting seeds: 45, 68, 44, 55, 68, 63, 23
 
       Console.WriteLine("\nGenerating " + numRows +
         " artificial data items with " + numFeatures + " features");
